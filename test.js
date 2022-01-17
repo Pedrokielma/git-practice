785f7b87f497751eb64664859510bbf536acae42
PedroKielma <peterkielma@gmail.com>
2022-01-17 16:41:38 +0000

    add test.js day 1

---
+++ test.js
@@ -0,0 +1,27 @@
+console.log("hello");
+let i = 0;
+// while (i<30){
+//     i++;
+//     if (i == 10){
+//         console.log ("ten")
+//     }else if (i == 20){
+//         console.log ("twenty")
+//     }else if (i == 30){
+//         console.log ("thirty")
+//     }else {console.log(i)}
+
+// }
+// let count = 0;
+// while (count < 5) {
+//     count++;
+//   console.log(count)
+  
+// }
+
+for(let i =0; i <= 20; i++){
+    if ( i %2 == 0){
+        console.log(i +" is even")
+    }else if(i %2 ==1){
+        console.log ( i+ " is odd")
+    }
+}
